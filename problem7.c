391955ab6877f2071ac90e131b3d55cd88f61855
Dharanesh <cdharanesh2006@gmail.com>
2026-02-08 23:25:37 +0530

    Add files via upload

--- problem7.c
+++ problem7.c
@@ -1,6 +1,22 @@
-#include<stdio.h>
-int main()
-{
-     printf("%d",sizeof(double));
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int rain, total = 0, heavyDays = 0;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &rain);
+        total += rain;
+
+        if(rain > 50) {
+            heavyDays++;
+        }
+    }
+
+    printf("Total Rainfall: %d\n", total);
+    printf("Heavy Rain Days: %d", heavyDays);
+
     return 0;
 }
--- problem8.c
+++ problem8.c
@@ -1,6 +1,22 @@
-#include<stdio.h>
-int main()
-{
-     printf("%d",sizeof(char));
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int loss, totalLoss = 0, highLossDays = 0;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &loss);
+        totalLoss += loss;
+
+        if(loss > 100) {
+            highLossDays++;
+        }
+    }
+
+    printf("Total Loss: %d\n", totalLoss);
+    printf("High Loss Days: %d", highLossDays);
+
     return 0;
 }
