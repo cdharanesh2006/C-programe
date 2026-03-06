c6127a6bcabdbd0c580266110fab54c573d9662e
Dharanesh <cdharanesh2006@gmail.com>
2026-03-06 09:32:00 +0530

    Add files via upload

--- problem6.c
+++ problem6.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    char str[100];
-    int i = 0, count = 0;
-
-    printf("Enter text: ");
-    scanf("%s", str);
-
-    while(str[i] != '\0') {
-        char ch = str[i];
-        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
-           ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
-            count++;
-        }
-        i++;
+int factorial(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
     }
+    return fact;
+}
 
-    printf("%d", count);
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", factorial(n));
     return 0;
 }
--- problem7.c
+++ problem7.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    char str[100];
-    int i = 0, count = 0;
+void checkPrime(int n) {
+    int i, flag = 1;
 
-    printf("Enter text: ");
-    scanf("%s", str);
-
-    while(str[i] != '\0') {
-        char ch = str[i];
-
-        if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
-            if(!(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
-                 ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')) {
-                count++;
-            }
-        }
-        i++;
+    if(n <= 1) {
+        printf("Not Prime");
+        return;
     }
 
-    printf("%d", count);
+    for(i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag == 1)
+        printf("Prime");
+    else
+        printf("Not Prime");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    checkPrime(n);
     return 0;
 }
