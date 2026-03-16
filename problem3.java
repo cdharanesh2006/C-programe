6844caf6c787b6fccd8cc555105df044539ee09e
Dharanesh <cdharanesh2006@gmail.com>
2026-03-16 13:38:25 +0530

    Add files via upload

--- problem3.java
+++ problem3.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
+
 public class problem3 {
-    public static void main(String[] args){
-        Scanner scan=new Scanner(System.in);
-        int tuitionFees = scan.nextInt();
-        int scholarship = scan.nextInt();
-        int examFee = scan.nextInt();
-        int libraryFee = scan.nextInt();
-        int studentscholership=(scholarship*tuitionFees)/100;
-        int finalFee = tuitionFees - studentscholership+ examFee + libraryFee;
-        System.out.println("Final Payble Fee="+finalFee);
-        scan.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int workingHours = sc.nextInt();
+        int attendancePercentage = sc.nextInt();
+
+        String result = (workingHours > 40 && attendancePercentage > 90) 
+                        ? "Eligible" 
+                        : "Not Eligible";
+
+        System.out.println(result);
     }
 }
