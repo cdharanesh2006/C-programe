9cb46edd39e35e9d239bc066f82c95738f9c9af9
Dharanesh <cdharanesh2006@gmail.com>
2026-03-16 13:43:22 +0530

    Add files via upload

--- problem4.java
+++ problem4.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
-public class GrocerySales {
+public class problem4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double sales = sc.nextDouble();
-        double discount = sc.nextDouble();
-        double gst = sc.nextDouble();
-        double discountAmount = (sales * discount) / 100;
-        double afterDiscount = sales - discountAmount;
-        double gstAmount = (afterDiscount * gst) / 100;
-        double finalRevenue = afterDiscount + gstAmount;
-        System.out.println("Final Revenue = " + finalRevenue);
+
+        int creditLimit = sc.nextInt();
+        int purchaseAmount = sc.nextInt();
+        int cardBlocked = sc.nextInt();
+
+        System.out.println(
+            ((purchaseAmount <= creditLimit) && (cardBlocked == 0)) 
+            ? "Approved" 
+            : "Declined"
+        );
+
         sc.close();
     }
 }
