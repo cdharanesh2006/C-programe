d507a3568352f177da8a61424176c12cd2d02b00
Dharanesh <cdharanesh2006@gmail.com>
2026-03-16 13:32:13 +0530

    Add files via upload

--- problem2.java
+++ problem2.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
+
 public class problem2 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        float baseCost = scan.nextFloat();
-        int extraGB = scan.nextInt();
-        int ratePerGB = scan.nextInt();
-        float tax = scan.nextFloat();
-        float extraCharge = extraGB * ratePerGB;
-        float taxAmount = (baseCost + extraCharge) * tax / 100;
-        float totalBill = baseCost + extraCharge + taxAmount;
-        System.out.println("Total Bill = " + totalBill);
-        scan.close();
+        Scanner sc = new Scanner(System.in);
+
+        int accountBalance = sc.nextInt();
+        int withdrawalAmount = sc.nextInt();
+        int dailyLimit = sc.nextInt();
+
+        if (withdrawalAmount <= accountBalance && withdrawalAmount <= dailyLimit) {
+            System.out.println("Approved");
+        } else {
+            System.out.println("Declined");
+        }
     }
 }
