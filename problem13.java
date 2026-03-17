fc44e4d33761bf3b9741e9dce3b2bf3586eaac40
Dharanesh <cdharanesh2006@gmail.com>
2026-03-17 13:37:25 +0530

    Add files via upload

--- problem13.java
+++ problem13.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
-public class problem13 {
-    public static void main(String[]args){
-        Scanner scan=new Scanner(System.in);
-        int yieldPerAcre=scan.nextInt();
-        int Acress=scan.nextInt();
-        int extraYield=scan.nextInt();
-        int damageCrops=scan.nextInt();
-        int value=(yieldPerAcre * Acress)+extraYield;
-        int Marketable=value-damageCrops;
-        System.out.println("Marketable Yield="+Marketable);
-        scan.close(); 
+import java.util.*;
 
+public class problem13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int connectionValue = sc.nextInt();
+        int applianceMask = sc.nextInt();
+
+        int result = connectionValue & applianceMask;
+
+        System.out.println((result > 0) ? "Appliance Active" : "Appliance Inactive");
     }
 }
