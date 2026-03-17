bb9805bb80d0913be8e6beaac95f17da76913268
Dharanesh <cdharanesh2006@gmail.com>
2026-03-17 13:27:11 +0530

    Add files via upload

--- problem7.java
+++ problem7.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class problem7 {
-    public static void main(String[]args){
-        Scanner scan=new Scanner(System.in);
-        double rodsPerHour=scan.nextDouble();
-        int hoursWorked=scan.nextInt();
-        double overtimeRods=scan.nextDouble();
-        double rejectRods=scan.nextDouble();
-        double production=(rodsPerHour * hoursWorked)+overtimeRods;
-        double Finalproduction=production-rejectRods;
-        System.out.println("Usable Rods="+(int)Finalproduction);
-        scan.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cartValue = sc.nextInt();
+        int premiumMember = sc.nextInt();
+
+        System.out.println((cartValue > 999 || premiumMember == 1) ? 
+        "Free Shipping" : "Shipping Charges Applied");
     }
 }
