ff22b2b91273f02233295bd288cff57d49072500
Dharanesh <cdharanesh2006@gmail.com>
2026-03-17 13:26:03 +0530

    Add files via upload

--- problem6.java
+++ problem6.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class problem6 {
-    public static void main(String[]args){
-        Scanner scan=new Scanner(System.in);
-         double distance = scan.nextDouble();
-        double mileage = scan.nextDouble();
-        double fuelPrice = scan.nextDouble();
-        double tollCharge = scan.nextDouble();
-        double fuelNeeded = distance / mileage;
-        double fuelCost = fuelNeeded * fuelPrice;
-        double totalCost = fuelCost + tollCharge;
-        System.out.println("Total Trip Cost = " + (int)totalCost);
-        scan.close();
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int familyIncome = scan.nextInt();
+        int percentage = scan.nextInt();
+
+        System.out.println((familyIncome < 200000 && percentage >= 75) ? 
+        "Scholarship Granted" : "Scholarship Not Granted");
     }
-    
 }
