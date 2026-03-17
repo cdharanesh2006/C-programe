6fc20352ac86d57a13a3ec0fa1e79b48f3c0d407
Dharanesh <cdharanesh2006@gmail.com>
2026-03-17 13:30:01 +0530

    Add files via upload

--- problem8.java
+++ problem8.java
@@ -1,15 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class problem8 {
-    public static void main(String[]args){
-        Scanner scan=new Scanner(System.in); 
-        int roomCharge=scan.nextInt();
-        int days=scan.nextInt();
-        int medicineCost=scan.nextInt();
-        int labFess=scan.nextInt();
-        int insuranceCoverage=scan.nextInt();
-        int total=(roomCharge * days) + medicineCost;
-        int total2=total+labFess-insuranceCoverage;
-        System.out.println("Payble Amount="+total2);
-        scan.close(); 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+        int idProof = sc.nextInt();
+
+        System.out.println((age >= 18 && idProof == 1) ? 
+        "Entry Allowed" : "Entry Denied");
     }
 }
