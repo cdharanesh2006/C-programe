20ac922aaf2cb09053dd9a3e65d1a417a11902cc
Dharanesh <cdharanesh2006@gmail.com>
2026-03-17 13:31:35 +0530

    Add files via upload

--- problem9.java
+++ problem9.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
-public class problem9 {
-    public static void main(String[]args){
-        Scanner scan=new Scanner(System.in);
-        int deliveries=scan.nextInt();
-        int payout=scan.nextInt();
-        int incentive=scan.nextInt();
-        int fuelCost=scan.nextInt();
-        int earnings=(deliveries*payout)+incentive;
-        int Netearnings=earnings-fuelCost;
-        System.out.println("Driver Earnings="+Netearnings);
-        scan.close();
 
+    import java.util.*;
+
+public class problem9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int usernameMatch = sc.nextInt();
+        int passwordMatch = sc.nextInt();
+        int accountLocked = sc.nextInt();
+
+        System.out.println((usernameMatch == 1 && passwordMatch == 1 && accountLocked != 1) ? 
+        "Login Success" : "Login Failed");
     }
 }
+
